a477e8e9811437d75182f762db35247c5aceab5a
David Giraldo <santiagogiraldosuescun@gmail.com>
2023-06-10 09:30:12 -0500

    Solucionamos un problema de versiones con Mockito y encima ya funciona el test de los dados

---
+++ src/main/java/com/platzi/javatestmockito/player/Player.java
@@ -0,0 +1,19 @@
+package com.platzi.javatestmockito.player;
+
+public class Player {
+
+    private Dice dice;
+    private  int nimNumberToWin;
+
+    public Player(Dice dice, int nimNumberToWin) {
+        this.dice = dice;
+        this.nimNumberToWin = nimNumberToWin;
+    }
+
+    public boolean play(){
+        int diceNumber =
+                dice.roll();
+        return diceNumber > nimNumberToWin;
+    }
+
+}
