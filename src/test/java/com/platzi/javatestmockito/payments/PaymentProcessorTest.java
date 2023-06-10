24433d7601bde8787e8b0895126f004900f64592
David Giraldo <santiagogiraldosuescun@gmail.com>
2023-06-10 14:52:26 -0500

    Identificamos las partes del test y aplicamos unas buenas practicas

--- src/test/java/com/platzi/javatestmockito/payments/PaymentProcessorTest.java
+++ src/test/java/com/platzi/javatestmockito/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.platzi.javatestmockito.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.*;
 
@@ -7,27 +8,35 @@ import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
 
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before//Sirve para decirle a JUnit que ejecute este metodo antes de cada test
+    public void setup(){
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
+
     @Test
     public void payment_is_correct() {
-
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
-        Mockito.when(paymentGateway.requestPayment(Mockito.any()))
+        //Preparacion de los objetos
+         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-
-        assertTrue(paymentProcessor.makePayment(1000));
+        //Ejecucion del metodo
+        boolean result = paymentProcessor.makePayment(1000);
+        //comprobacion del resultado
+        assertTrue(result);
 
     }
 
     @Test
     public void payment_is_wrong() {
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
 
